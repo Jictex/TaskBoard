7401cadb6758bf86a2abb041cea0e2cd13bf02fe
Jasper van Hardeveld <info@jictex.nl>
2019-03-04 00:06:26 +0100

    First commit

---
+++ src/column.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styled from "styled-components";
+import { Droppable, Draggable } from "react-beautiful-dnd";
+import Task from "./task";
+
+const Container = styled.div`
+  margin: 8px;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  width: 220px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding 8px;
+  transition: background-color 0.2s ease;
+  background-color: ${props => (props.isDraggingOver ? "lightgrey" : "white")};
+  flex-grow:1;
+  min-height: 100px;
+`;
+
+class InnerList extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.tasks === this.props.tasks) {
+      return false;
+    }
+    return true;
+  }
+  render() {
+    return this.props.tasks.map((task, index) => (
+      <Task key={task.id} task={task} index={index} />
+    ));
+  }
+}
+
+export default class Column extends React.Component {
+  render() {
+    return (
+      <Draggable draggableId={this.props.column.id} index={this.props.index}>
+        {provided => (
+          <Container {...provided.draggableProps} ref={provided.innerRef}>
+            <Title {...provided.dragHandleProps}>
+              {this.props.column.title}
+            </Title>
+            <Droppable
+              droppableId={this.props.column.id}
+              isDropDisabled={this.props.isDropDisabled}
+              type="task"
+            >
+              {(provided, snapshot) => (
+                <TaskList
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  isDraggingOver={snapshot.isDraggingOver}
+                >
+                  <InnerList tasks={this.props.tasks} />
+                  {provided.placeholder}
+                </TaskList>
+              )}
+            </Droppable>
+          </Container>
+        )}
+      </Draggable>
+    );
+  }
+}
